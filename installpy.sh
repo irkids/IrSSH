97b3465a3047069798382d383e0464c80b6461fa
irkids <147476857+irkids@users.noreply.github.com>
2024-09-06 22:55:54 +0330

    Update installpy.sh

--- installpy.sh
+++ installpy.sh
@@ -4,11 +4,11 @@
 sudo apt update
 sudo apt install -y python3 python3-pip mysql-server
 
-# Link to the Python script from GitHub (replace this with your actual link)
-https://raw.githubusercontent.com/irkids/IrSSH/main/install.py="https://raw.githubusercontent.com/irkids/IrSSH/main/install.py"
+# Link to the Python script from GitHub
+PYTHON_SCRIPT_URL="https://raw.githubusercontent.com/irkids/IrSSH/main/install.py"
 
 # Downloading Python script
-wget $https://raw.githubusercontent.com/irkids/IrSSH/main/install.py -O /tmp/install.py
+wget $PYTHON_SCRIPT_URL -O /tmp/install.py
 
 # Running the Python script
 python3 /tmp/install.py
