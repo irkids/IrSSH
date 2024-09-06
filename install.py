c0bafbbc8fe6f29bd6caf335779549039acb839a
irkids <147476857+irkids@users.noreply.github.com>
2024-09-06 22:50:08 +0330

    Update install.py

--- install.py
+++ install.py
@@ -1,129 +1,63 @@
-#!/usr/bin/env python3
 import os
-import sys
-import time
-import subprocess
-import re
-import random
-import datetime
 
-# hostname ShahanPanel.link
-with open('/etc/resolv.conf', 'w') as f:
-    f.write("#shahanDNS\nnameserver 8.8.8.8\n")
+def install_tuic():
+    print("Installing TUIC...")
+    os.system("git clone https://github.com/tuic/tuic.git")
+    os.system("cd tuic && ./install.sh")
 
-def printshahan(text, delay):
-    for char in text:
-        sys.stdout.write(char)
-        sys.stdout.flush()
-        time.sleep(float(delay))
-    print()
+def install_shadowsocks():
+    print("Installing Shadowsocks...")
+    os.system("apt install -y shadowsocks-libev")
 
-def isRoot():
-    return os.geteuid() == 0
+def install_wireguard():
+    print("Installing WireGuard...")
+    os.system("apt install -y wireguard")
 
-if not isRoot():
-    print("Sorry, you need to run this as root")
-    sys.exit(1)
+def install_l2tp_ipsec():
+    print("Installing L2TP/IPSec...")
+    os.system("apt install -y strongswan xl2tpd")
 
-os.environ['PATH'] += ':/usr/local/bin'
+def install_ikev2_ipsec():
+    print("Installing IKEv2/IPSec...")
+    os.system("apt install -y strongswan")
 
-panelporttmp = subprocess.check_output("sudo lsof -i -P -n | grep -i LISTEN | grep apache2 | awk '{if(!seen[$9]++)print $9;exit}'", shell=True).decode().strip()
-panelportt = re.sub(r'[^0-9]', '', panelporttmp)
+def install_openvpn():
+    print("Installing OpenVPN...")
+    os.system("apt install -y openvpn")
 
-subprocess.run("sed -i 's/#Port 22/Port 22/' /etc/ssh/sshd_config", shell=True)
-po = subprocess.check_output("cat /etc/ssh/sshd_config | grep '^Port'", shell=True).decode().strip()
-port = po.replace("Port ", "")
+def install_singbox():
+    print("Installing Sing-box...")
+    os.system("git clone https://github.com/SagerNet/sing-box.git")
+    os.system("cd sing-box && ./install.sh")
 
-adminuser = subprocess.check_output("mysql -N -e \"use ShaHaN; select adminuser from setting where id='1';\"", shell=True).decode().strip()
-adminpass = subprocess.check_output("mysql -N -e \"use ShaHaN; select adminpassword from setting where id='1';\"", shell=True).decode().strip()
+def main():
+    print("Select the protocol to install:")
+    print("1. TUIC")
+    print("2. Shadowsocks")
+    print("3. WireGuard")
+    print("4. L2TP/IPSec")
+    print("5. IKEv2/IPSec")
+    print("6. OpenVPN")
+    print("7. Sing-box")
+    
+    choice = input("Enter the number of the protocol: ")
 
-subprocess.Popen("sudo wget -4 -O /usr/local/bin/shahan https://raw.githubusercontent.com/HamedAp/Ssh-User-management/main/screenshot/shahan", shell=True)
-subprocess.run("sudo chmod a+rx /usr/local/bin/shahan", shell=True)
+    if choice == "1":
+        install_tuic()
+    elif choice == "2":
+        install_shadowsocks()
+    elif choice == "3":
+        install_wireguard()
+    elif choice == "4":
+        install_l2tp_ipsec()
+    elif choice == "5":
+        install_ikev2_ipsec()
+    elif choice == "6":
+        install_openvpn()
+    elif choice == "7":
+        install_singbox()
+    else:
+        print("Invalid selection")
 
-subprocess.Popen("sudo wget -4 -O /usr/local/bin/shahancheck https://raw.githubusercontent.com/HamedAp/Ssh-User-management/main/screenshot/shahancheck", shell=True)
-subprocess.run("sudo chmod a+rx /usr/local/bin/shahancheck", shell=True)
-
-subprocess.Popen("sudo wget -4 -O /root/tls.sh.x https://github.com/HamedAp/Ssh-User-management/raw/main/tls.sh.x", shell=True)
-subprocess.run("sudo chmod a+rx /root/tls.sh.x", shell=True)
-
-subprocess.Popen("sudo wget -4 -O /root/shadow.sh.x https://github.com/HamedAp/Ssh-User-management/raw/main/shadow.sh.x", shell=True)
-subprocess.run("sudo chmod a+rx /root/shadow.sh.x", shell=True)
-
-subprocess.Popen("sudo wget -4 -O /root/signbox.sh.x https://github.com/HamedAp/Ssh-User-management/raw/main/signbox.sh.x", shell=True)
-subprocess.run("sudo chmod a+rx /root/signbox.sh.x", shell=True)
-
-subprocess.Popen("sudo wget -4 -O /root/updatesignbox.sh.x https://github.com/HamedAp/Ssh-User-management/raw/main/updatesignbox.sh.x", shell=True)
-subprocess.run("sudo chmod a+rx /root/updatesignbox.sh.x", shell=True)
-
-if not subprocess.run("grep -q -E '^shahansources$' /etc/apt/sources.list", shell=True).returncode:
-    print("all good, do nothing")
-else:
-    subprocess.run("sudo sed -i '/shahansources/d' /etc/apt/sources.list", shell=True)
-    subprocess.run("sudo sed -i '/ubuntu focal main restricted universe/d' /etc/apt/sources.list", shell=True)
-    subprocess.run("sudo sed -i '/ubuntu focal-updates main restricted universe/d' /etc/apt/sources.list", shell=True)
-    subprocess.run("sudo sed -i '/ubuntu focal-security main restricted universe multiverse/d' /etc/apt/sources.list", shell=True)
-    subprocess.run("sudo sed -i '/ubuntu focal partner/d' /etc/apt/sources.list", shell=True)
-    with open('/etc/apt/sources.list', 'a') as f:
-        f.write("""#shahansources
-deb http://archive.ubuntu.com/ubuntu focal main restricted universe
-deb http://archive.ubuntu.com/ubuntu focal-updates main restricted universe
-deb http://security.ubuntu.com/ubuntu focal-security main restricted universe multiverse
-deb http://archive.canonical.com/ubuntu focal partner
-""")
-
-os.system('clear')
-print()
-printshahan("ShaHaN Panel Installation :) By HamedAp", 0.1)
-print("\n")
-printshahan("Please Wait . . .", 0.1)
-print("\n")
-
-if adminuser:
-    adminusername = adminuser
-    adminpassword = adminpass
-else:
-    adminusername = "admin"
-    print("\nPlease input Panel admin user.")
-    usernametmp = input(f"Default user name is \033[33m{adminusername}\033[0m, let it blank to use this user name: ")
-    if usernametmp:
-        adminusername = usernametmp
-    adminpassword = "123456"
-    print("\nPlease input Panel admin password.")
-    passwordtmp = input(f"Default password is \033[33m{adminpassword}\033[0m, let it blank to use this password : ")
-    if passwordtmp:
-        adminpassword = passwordtmp
-
-file = '/etc/systemd/system/videocall.service'
-if os.path.exists(file):
-    print("")
-else:
-    udpport = "7300"
-    print("\nPlease input UDPGW Port .")
-    udpport = input(f"Default Port is \033[33m{udpport}\033[0m, let it blank to use this Port: ") or udpport
-
-ipv4 = subprocess.check_output("curl -s ipv4.icanhazip.com", shell=True).decode().strip()
-
-subprocess.Popen("sudo sed -i '/www-data/d' /etc/sudoers", shell=True)
-subprocess.Popen("sudo sed -i '/apache/d' /etc/sudoers", shell=True)
-
-subprocess.run("sed -i 's@#Banner none@Banner /var/www/html/p/banner.txt@' /etc/ssh/sshd_config", shell=True)
-subprocess.run("sed -i 's@#PrintMotd yes@PrintMotd yes@' /etc/ssh/sshd_config", shell=True)
-subprocess.run("sed -i 's@#PrintMotd no@PrintMotd yes@' /etc/ssh/sshd_config", shell=True)
-
-# The rest of the script follows a similar pattern, converting Bash commands to their Python equivalents.
-# Due to the length of the script, I'll summarize the remaining operations:
-
-# 1. Various system updates and package installations
-# 2. Database operations
-# 3. File downloads and permission changes
-# 4. Configuration file modifications
-# 5. Cron job setups
-# 6. Final system checks and information display
-
-# The script ends with printing the panel information:
-
-print(f"\n\n\nPanel Link : http://{ipv4}/p")
-print(f"UserName : \033[31m{adminusername}\033[0m")
-print(f"Password : \033[31m{adminpassword}\033[0m")
-print(f"Port : \033[31m{port}\033[0m")
-print(f"\nNOW You Can Use \033[32mshahan\033[0m and \033[32mshahancheck\033[0m Command To See Menu Of Shahan Panel")
+if __name__ == "__main__":
+    main()
